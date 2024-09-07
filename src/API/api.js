e5bd094982b835d19ed55e67ac57a78f0452afc6
kangeunhwa <dmdgi37@naver.com>
2024-09-07 21:00:44 +0900

    portnumber 수정

--- src/API/api.js
+++ src/API/api.js
@@ -2,7 +2,9 @@ import { API_AXIOS } from "./axios";
 
 export const Search = async (searchValue) => {
   try {
-    const data = await API_AXIOS.post(`/search`, { fullName: searchValue });
+    const data = await API_AXIOS.post(`http://34.64.95.196:8080/search`, {
+      fullName: searchValue,
+    });
     return data.data;
   } catch (error) {
     console.error(error);
